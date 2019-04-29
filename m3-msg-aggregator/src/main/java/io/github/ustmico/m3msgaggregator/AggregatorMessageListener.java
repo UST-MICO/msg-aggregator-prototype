8d0a813c96474e593740177a9910aef3985f72f6
wagnerdk <18245675+wagnerdk@users.noreply.github.com>
2019-04-29 22:47:17 +0200

    Add logging message listener

--- m3-msg-aggregator/src/main/java/io/github/ustmico/m3msgaggregator/AggregatorMessageListener.java
+++ m3-msg-aggregator/src/main/java/io/github/ustmico/m3msgaggregator/AggregatorMessageListener.java
@@ -4,8 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.cloudevents.CloudEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.LinkedList;
+import java.util.List;
+
 @Slf4j
 public class AggregatorMessageListener {
+
+    /**
+     * At the moment the list of open aggregates (incomplete set of messages)
+     * are stored in memory. TODO Implement a database variant
+     */
+    List<List<CloudEvent<JsonNode>>> activeAggregate = new LinkedList<>();
 
     @KafkaListener(topics = "${kafka.inputTopic}")
     public void receive(CloudEvent<JsonNode> cloudEvent) {
